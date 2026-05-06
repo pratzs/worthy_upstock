09946bd42bf1ba83e46897ea3cfffa6f70036418
pratzs <pratzs@live.com>
2026-05-06 13:48:39 +1200

    Add __manifest__.py for Worthy Upstock Integration

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Worthy Upstock Integration',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Receives webhooks from Upstock via Svix',
+    'depends': ['base', 'sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
